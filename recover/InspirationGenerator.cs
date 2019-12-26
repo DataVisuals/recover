c99e9d475d9e697be6052aab768f54188741ff36
sprucea <andrew.spruce@gmail.com>
2019-12-26 21:08:05 +0000

    Save only the quote generator and abandon other changes

---
+++ recover/InspirationGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace recover
+{
+    public class InspirationGenerator
+    {
+
+        public static string[] inspires = {
+            "Waste isn't wast until you waste it - Will.i.am",
+            "Buy less, choose well - Vivienne Westwood",
+            "The greatest threat to our planet is the belief that someone else will save it. - Robert Swan"
+        };
+
+        public string getQuote()
+        {
+            string quote = null;
+            Random rnd = new Random();
+            int idx = rnd.Next(0,inspires.Length);
+            quote = inspires[idx];
+            return quote;
+        }
+    }
+}
